fa863af5f068e3910c76034712e39f2ff9cfafde
aarneranta <aarne@chalmers.se>
2020-05-21 15:59:41 +0200

    mini versions of query and draw

--- python/query.py
+++ python/query.py
@@ -1,8 +1,10 @@
 # a simple query system for arithmetic, using Montague-style semantics
 # and an embedded GF grammar
-# examples: "is 131 prime", "which numbers are prime"
+# examples: "is 131 prime", "which numbers are prime", "365 * 24 * 60"
+# presupposes 'gf -make QueryEng (QuerySwe ...)'
 
 import pgf
+import sys
 
 # we assume this abstract syntax
 absmodule = "Query"
@@ -14,10 +16,11 @@ langname = absmodule + "Eng"
 allNumbers = range(1,10000)
 
 def answer(tree):
+  "top level Query interpreter"
   fun,args = tree.unpack()
   
   if fun == "QWhich":
-    ans = enumerate(lambda x: predicate(args[0])(x) and predicate(args[1])(x))
+    ans = enumerate(lambda x: predicate(args[0])(x) and predicate(args[1])(x), allNumbers)
   elif fun == "QWhether":
     ans = quantifier(args[0])(predicate(args[1]))
   else: # QWhat
@@ -25,8 +28,8 @@ def answer(tree):
     
   return str(ans)
 
-# returns int -> bool
 def predicate(property):
+  "takes Property or Kind to int -> bool"
   prop,pargs = property.unpack()
   if prop == "PAnd":
     return lambda x: predicate(pargs[0])(x) and predicate(pargs[1])(x)
@@ -58,20 +61,20 @@ def predicate(property):
   else:
     print("not a valid property",property)
 
-# returns (int -> bool) -> bool
 def quantifier(term):
+  "takes Term to (int -> bool) -> bool"
   fun,args = term.unpack()
   if fun == "TElement":
     return lambda p: p(value(args[0]))
   elif fun == "TAll":
-    return lambda p: forAll(lambda x: not predicate(args[0])(x) or p(x))
+    return lambda p: forAll(lambda x: not predicate(args[0])(x) or p(x), allNumbers)
   elif fun == "TAny":
-    return lambda p: forSome(lambda x: predicate(args[0])(x) and p(x))
+    return lambda p: forSome(lambda x: predicate(args[0])(x) and p(x), allNumbers)
   else:
     print("not a valid term",term)
 
-# returns int
 def value(element):
+  "takes Element to int"
   fun,args = element.unpack()
   if fun == "EInteger":
     return int(args[0].unpack())
@@ -89,6 +92,7 @@ def value(element):
     print("not a valid element",element)
   
 def prime(n):
+  "simple way to decide if a positive number is prime"
   if n == 1:
     r = False
   else:
@@ -97,32 +101,41 @@ def prime(n):
     if n % k == 0: return False
   return r
 
-def forAll(p):
-  for n in allNumbers:
+def forAll(p,ns):
+  "universal quantifier over a list"
+  for n in ns:
     if not p(n): return False
   return True
     
-def forSome(p):
-  for n in allNumbers:
+def forSome(p,ns):
+  "existential quantifier over a list"
+  for n in ns:
     if p(n): return True
   return False
 
-def enumerate(p):
+def enumerate(p,ns):
+  "filter over a list"
   r = []
-  for n in allNumbers:
+  for n in ns:
     if p(n): r = r + [n]
   return r
 
 def factorial(n):
+  "factorial of positive numbers"
   r = 1
   for k in range(1,n+1):
     r = k*r
   return r
     
 def main():
+  "main function to be called from stdio, language code as optional argument"
+  
   # read in the grammar, set up the input language
   grammar = pgf.readPGF(absmodule + ".pgf")
-  lang    = grammar.languages[langname]
+  langcode = langname
+  if len(sys.argv) > 1:
+    langcode = absmodule + sys.argv[1]
+  lang = grammar.languages[langcode]
 
   # read a line of input, parse in lang, return answer
   line  = input("")
